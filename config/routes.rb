d4d57a071eb4d424a3eb754350ff529a1cd791b8
pipopotamasu <ec0156hx@gmail.com>
2018-03-16 22:38:26 +0900

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/chat_messages', to: 'chat_messages#index'
+  root to: 'chat_messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
