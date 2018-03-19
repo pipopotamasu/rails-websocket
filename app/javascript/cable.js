c2021aaf6810f2ee656b2a729ce6ce609c54b70d
pipopotamasu <ec0156hx@gmail.com>
2018-03-19 23:03:27 +0900

    moved to webpacker

--- app/javascript/cable.js
+++ app/javascript/cable.js
@@ -1,7 +1,26 @@
 import ActionCable from '../../vendor/bundle/ruby/2.4.0/gems/actioncable-5.1.5/lib/assets/compiled/action_cable'
-console.log(ActionCable)
-// import './channels/chat_message.coffee'
 
-// this.App || (this.App = {});
+let App = {}
 
-// App.cable = ActionCable.createConsumer();
+App.cable = ActionCable.createConsumer();
+
+App.chat_message = App.cable.subscriptions.create("ChatMessageChannel", {
+  connected: function() {},
+  disconnected: function() {},
+  received: function(data) {
+    return $('#chat_messages').append('<div>' + data['message'] + '</div>');
+  },
+  speak: function(message) {
+    return this.perform('speak', {
+      message: message
+    });
+  }
+});
+
+$(document).on('keypress', '[data-behavior~=speak_chat_messages]', function(event) {
+  if (event.keyCode === 13) {
+    App.chat_message.speak(event.target.value);
+    event.target.value = '';
+    return event.preventDefault();
+  }
+});
