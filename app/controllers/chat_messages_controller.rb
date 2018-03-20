dea474d157f8921620fc74e417759c449016b929
pipopotamasu <ec0156hx@gmail.com>
2018-03-20 19:39:15 +0900

    add vue routing

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -2,4 +2,8 @@ class ChatMessagesController < ApplicationController
   def index
     @chat_messages = ChatMessage.all
   end
+
+  def index_vue
+    @chat_messages = ChatMessage.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/chat_messages', to: 'chat_messages#index'
+  get '/vue_chat_messages', to: 'chat_messages#index_vue'
   root to: 'chat_messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
