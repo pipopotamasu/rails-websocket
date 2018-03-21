f70cc1fd58a918f56fdf9c3a96fe53f1d03b1fe8
pipopotamasu <ec0156hx@gmail.com>
2018-03-21 12:38:17 +0900

    finish websocket func

--- app/javascript/components/ChatMessages.vue
+++ app/javascript/components/ChatMessages.vue
@@ -6,7 +6,7 @@
     <br/>
     <form>
       <label>
-        発言する: <input type="text" data-behavior="speak_chat_messages" v-model="message" @keypress.enter.prevent="createMessage()">
+        発言する: <input type="text" data-behavior="speak_chat_messages" v-model="input_message" @keypress.enter.prevent="createMessage()">
       </label>
     </form>
   </div>
@@ -14,22 +14,42 @@
 
 <script>
 import ActionCable from '../../../vendor/bundle/ruby/2.4.0/gems/actioncable-5.1.5/lib/assets/compiled/action_cable'
+import $ from 'jquery'
 
 export default {
   data () {
     return {
-      message: ''
+      input_message: '',
+      messages: this.init_messages,
+      connection: {}
     }
   },
   props: {
-    messages: {
+    init_messages: {
       type: Array,
       required: true
     }
   },
+  created () {
+    let vueInstance = this
+    this.connection = ActionCable.createConsumer().subscriptions.create("ChatMessageChannel", {
+      connected: function() {},
+      disconnected: function() {},
+      received: function(data) {
+        vueInstance.messages.push(data['message'])
+      },
+      speak: function(message) {
+        return this.perform('speak', {
+          message: message
+        })
+      }
+    })
+  },
   methods: {
     createMessage () {
-      this.message = ''
+      if (this.input_message == '') return false
+      this.connection.speak(this.input_message)
+      this.input_message = ''
     }
   }
 }
