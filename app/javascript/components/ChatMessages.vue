52e4f8d0c8b28a8ad08fff4d5152835cd2223a31
pipopotamasu <ec0156hx@gmail.com>
2018-03-21 12:21:47 +0900

    event bind

--- app/javascript/components/ChatMessages.vue
+++ app/javascript/components/ChatMessages.vue
@@ -6,7 +6,7 @@
     <br/>
     <form>
       <label>
-        発言する: <input type="text" data-behavior="speak_chat_messages">
+        発言する: <input type="text" data-behavior="speak_chat_messages" v-model="message" @keypress.enter.prevent="createMessage()">
       </label>
     </form>
   </div>
@@ -16,14 +16,21 @@
 import ActionCable from '../../../vendor/bundle/ruby/2.4.0/gems/actioncable-5.1.5/lib/assets/compiled/action_cable'
 
 export default {
+  data () {
+    return {
+      message: ''
+    }
+  },
   props: {
     messages: {
       type: Array,
       required: true
     }
   },
-  created () {
-    console.log(this.messages)
+  methods: {
+    createMessage () {
+      this.message = ''
+    }
   }
 }
 </script>
